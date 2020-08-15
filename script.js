40c00e881c574596e23cc9328ef7944b7843bec2
Naim Sedán <naim.alejandro.sedan@gmail.com>
2020-08-15 01:16:32 +0100

    initial level array finished

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-let level = 1;
-let clicked = [];
+let level = 0;
+let clicked = []
+const btnArray = ["#red", "#blue", "#green", "#orange"]
+let levelArray = []
 
+const stringify = JSON.stringify
+const parse = JSON.parse
 
-//btn ids
-function getButtons() {
-    return $('.button-container').find('.button').map(function () {
-        return '#' + this.id;
-    }).toArray();
+// INITIAL LEVEL
+for (let i = 1; i <= 5; i++) {
+    let random = Math.floor(btnArray.length * Math.random());
+    let randomBtn = btnArray[random]
+    levelArray.push(randomBtn)
 }
-
-const btnArray = getButtons();
-
-// random btn
-function random(min, max) {
-    return min + Math.floor((max - min) * Math.random());
-}
-
-function makeArrayFrom(number) {
-    return Array.from(Array(number).keys());
-}
+console.log(levelArray);
 
 
-function levelArray() {
-    return makeArrayFrom(5).map(function () {
-        const number = random(0, btnArray.length);
-        return btnArray[number]
-    });
-}
 
-console.log(levelArray())
 
-let levelArrayVar = levelArray()
-let fullArray = [...levelArrayVar]
 
-console.log(fullArray)
-
-function newLevel(){
-    return fullArray.slice(0,level)
-}
-
-let currentLevel = newLevel()
-
+let currentLevel = stringify(levelArray.splice(btnArray.length - level))
 console.log(currentLevel)
 
 
 
-// current level
+
+
+// checking player clicks
+function checking() {
+    if (stringify(clicked).lenght === currentLevel.lenght) {
+        if (stringify(clicked) === currentLevel) {
+            console.log('%c Level Complete!', 'background: green; color: white')
+            //NEXT LEVEL
+            level++
+            console.log(level)
+            console.log(currentLevel)
+            return clicked.pop()
+        }
+        else {
+            console.log('%c Wrong!', 'background: red; color: white')
+            clicked.length = 0;
+            level = 1 //strict mode
+            return
+        }
+
+    }
+}
 
 
 // player clicks
@@ -65,31 +64,3 @@ $(btnArray[3]).click(function () {
     clicked.push('#' + this.id);
     checking()
 });
-
-
-// checking player clicks
-function checking() {
-    const toString = JSON.stringify
-    if (toString(clicked).lenght === toString(currentLevel.lenght)) {
-        if (toString(clicked) === toString(currentLevel)) {
-            console.log('%c Level Complete!', 'background: green; color: white')
-            level++
-            currentLevel
-            console.log(newLevel())
-            return clicked.pop()
-        }
-        else {
-            console.log('%c Wrong!', 'background: red; color: white')
-            clicked.length = 0;
-            console.log(newLevel())
-            level = 1 //strict mode
-            return
-        }
-
-    } 
-}
-
-
-// reload page to switch off
-//location.reload();
-
